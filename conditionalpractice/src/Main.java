e74a86701a360fb668d5dc2c861ce4b20dcce306
Mursal <mursalomari111@gmail.com>
2025-09-22 08:51:48 -0400

    Update Main.java

--- conditionalpractice/src/Main.java
+++ conditionalpractice/src/Main.java
@@ -13,31 +13,31 @@ public class Main {
         //get a line of input convert and save the input into an integer variable (you can assume the user enters a  valid whole number)
         int userInput= myScanner.nextInt();
         //write an if statement that prints "hello world" if the input was less than 4
-        if (x<4){
+        if (userInput<4){
             System.out.println("hello world");
         }
         //write an if statement that prints out "Why did the man fall down the well?" if the input was less than 5
-        if (x<5){
+        if (userInput<5){
             System.out.println("Why did the man fall down the well?");
         }
         //write an if statement that prints out "Because he couldn't see that well." if the input was greater than 1
-        if (x>1){
+        if (userInput>1){
             System.out.println("Because he couldn't see that well.");
         }
         //write an if statement that prints out "When does a joke become a dad joke?"  if the input was equal to 8
-        if (x==8){
+        if (userInput==8){
             System.out.println("When does a joke become a dad joke?");
         }
         //write an if statement that prints out "When it becomes apparent."  if the input was greater than or equal to 8
-        if (x>=8){
+        if (userInput>=8){
             System.out.println("When it becomes apparent.");
         }
         //write an if statement that prints out "I used to hate facial hair," if the input is less than or equal to 12
-        if (x<=12){
+        if (userInput<=12){
             System.out.println("I used to hate facial hair,");
         }
         //write an if statement that prints out " but then it grew on me."  if the input does not equal 23
-        if (x!=23){
+        if (userInput!=23){
             System.out.println(" but then it grew on me.");
         }
 
